bb1679a661cf3f55a36ddcb19e4e1bc978ccb2bc
Jack Crish <jackcrish@MBA-Jack.local>
2018-11-22 16:50:28 -0500

    better example of mapping and template loops

--- usingTemplates.js
+++ usingTemplates.js
@@ -1,29 +1,18 @@
-//Render Article Titles and Headings
-let renderMain = (heading, week, options, pay) => {
-  return `<form>
-  <h4>${heading}</h4>
-  <div>
-  <form id="schedule">
-    <select>
-      <option>Monday</option>
-      <option>Tuesday</option>
-      <option>Wednesday</option>
-      <option>Thursday</option>
-      <option>Friday</option>
-      <option>Sunday</option>
-    </select>
-  </form>
-  </div>
-  <h4>${week}</h4>
-  ${Array(5).fill().map( (item, i) => ` 
-    <p>I am item ${item} index number ${i}</p>
-  `)}
-  `
-}
 
 //Render Nav Links
 let renderNav = (href, title) => {
   return `<a href=${href}><p>${title}</p></a>`
+}
+
+
+//use this as a reference when you're using loops in 
+// tempates: https://gist.github.com/wiledal/3c5b63887cc8a010a330b89aacff2f2e
+let renderMain = (data) => {
+  return `<p>${data.heading}</p>  
+    ${data.options.map( (item, i) => `
+      <p> ${item} </p>
+    `).join("")}
+  `
 }
 
 //#################################################
@@ -38,16 +27,16 @@ let linkNames = [
 //Built in data
 let mainData = [
   {
-      heading: "Day",
-      options: ["Monday, Tuesday, Wednesday, Thursday, Friday, Sunday"]
+    heading: "Day",
+    options: ["Monday, Tuesday, Wednesday, Thursday, Friday, Sunday"]
   },
   {
-      heading: "Week",
-      options: [40, 41, 42, 43, 44, 45, 46]
+    heading: "Week",
+    options: [40, 41, 42, 43, 44, 45, 46]
   },
   {
-      heading: "Time",
-      options: ["11:00", "12:00", "1:00", "2:00", "3:00", "4:00"]
+    heading: "Time",
+    options: ["11:00", "12:00", "1:00", "2:00", "3:00", "4:00"]
   }
 ];
 //END TEMP DATA SOURCE
@@ -56,11 +45,14 @@ let mainData = [
 //Capture the main element and build articles
 let main = document.querySelector(".main").querySelector('section');
 let nav = document.querySelector(".nav").querySelector('section');
-  
-mainData.map( (item) => {
-  main.innerHTML += renderMain(item.heading, item.articleInfo, item.videoLink)
+
+//capture each object in the array
+//since data is an array objects we will map over each element in the array.
+main.innerHTML = mainData.map( i => {
+  return renderMain(i);
 })
 
+//render the nav bar based on the temporary data we have stored above. 
 linkNames.map( (item) => {
   console.log("item ", item)
   nav.innerHTML += renderNav(item.linkLink, item.linkTitle)
